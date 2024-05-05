ad1e289ad5a3d5c88539c44088312ab56d4c0cf2
AdamRogres <99787197+AdamRogres@users.noreply.github.com>
2024-05-05 16:26:09 -0700

    Fixed issue with the config file

--- src/main/java/com/adamsmods/adamsarsplus/Config.java
+++ src/main/java/com/adamsmods/adamsarsplus/Config.java
@@ -7,4 +7,12 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = AdamsArsPlus.MOD_ID)
 public class Config {
 
+    public static ForgeConfigSpec SERVER_CONFIG;
+    static {
+        ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
+
+        SERVER_BUILDER.comment("SERVER CONFIGURATION");
+
+        SERVER_CONFIG = SERVER_BUILDER.build();
+    }
 }
